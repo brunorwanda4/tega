4188902c0ffacd42124ba7fd6365ef82a3baff2b
brunorwanda4 <brunorwanda4@gmail.com>
2026-03-15 17:07:55 +0200

    fix bug in application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import AuthLogo from "./(auth)/_components/auth-logo";
 
 export default function Home() {
   return (
     <div>
-      <div>Tega </div>
+      <div>Tega home </div>
     </div>
   );
 }
